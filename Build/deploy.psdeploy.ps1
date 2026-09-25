2249f24d34898dfa334d426437372d601d6db889
Mark Wragg <mark@wragg.io>
2026-09-25 12:24:44 +0100

    Update deployment scripts to reference 'master' branch instead of 'main'

--- Build/deploy.psdeploy.ps1
+++ Build/deploy.psdeploy.ps1
@@ -14,7 +14,7 @@ else {
 if (
     $ModuleSourcePath -and
     $env:BHBuildSystem -ne 'Unknown' -and
-    $env:BHBranchName -eq "main" -and
+    $env:BHBranchName -eq "master" -and
     $ENV:NugetApiKey
 ) {
     Deploy Module {
@@ -29,7 +29,7 @@ if (
 } else {
     "Skipping deployment: To deploy, ensure that...`n" +
     "`t* You are in a known build system (Current: $ENV:BHBuildSystem)`n" +
-    "`t* You are committing to the main branch (Current: $ENV:BHBranchName) `n" +
+    "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" +
     "`t* You have access to the Nuget API key" |
         Write-Host
 }
